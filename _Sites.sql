4b6036f65d6c57b173462b816f74b1eebaaaa7ff
Johnard Hidalgo <jhidalgo@hedgeserv.com>
2024-07-10 11:22:10 +0800

    commit

---
+++ _Sites.sql
@@ -0,0 +1,41 @@
+
+--GitHub
+https://github.com/KalobTaulien/example-repo#readme
+https://github.com/pcottle/learnGitBranching?tab=readme-ov-file
+https://learngitbranching.js.org/?NODEMO
+
+
+
+
+
+--How to Build a Professional Data Portfolio Website (Full Tutorial)
+https://www.youtube.com/watch?v=U69s3XXBMbg
+
+
+--Excel
+https://www.youtube.com/watch?v=dWVuw2smBhQ
+https://www.youtube.com/watch?v=McLQ3gEGbHQ
+
+
+
+--Python
+https://www.learnpython.org/en/Lists
+
+
+--SQL Exercises
+https://sqlpd.com/
+
+
+--AWS
+https://www.youtube.com/watch?v=C99ps_cWp9Y
+https://docs.aws.amazon.com/codepipeline/latest/userguide/concepts.html#concepts-pipelines
+https://docs.aws.amazon.com/codepipeline/latest/userguide/concepts-how-it-works.html
+
+
+--Data Analytsics
+https://grow.google/certificates/data-analytics/
+
+
+--Ansible
+https://www.youtube.com/watch?v=lVcvY4Wy6_8&list=PL2qzCKTbjutIyQAe3GglWISLnLTQLGm7e&index=17
+
