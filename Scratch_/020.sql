25484c23888c39fdeee603de2d88a2d5bcc6c865
Johnard Hidalgo <jhidalgo@hedgeserv.com>
2026-01-28 12:27:08 +0800

    commit

--- Scratch_/020.sql
+++ Scratch_/020.sql
@@ -1,10 +1,10 @@
 SELECT
-	'fds_gl_posting' AS TableName,
-	'id' AS ColumnName,
-	MAX([id]) AS CurrentMaxValue,
+	'tablename' AS TableName,
+	'columnname' AS ColumnName,
+	MAX([columnname]) AS CurrentMaxValue,
 	CASE
-		WHEN MAX([id]) <= 255 AND MIN([id]) >= 0 THEN 'TINYINT'
-		WHEN MAX([id]) <= 32767 AND MIN([id]) >= -32768 THEN 'SMALLINT'
+		WHEN MAX([columnname]) <= 255 AND MIN([columnname]) >= 0 THEN 'TINYINT'
+		WHEN MAX([columnname]) <= 32767 AND MIN([columnname]) >= -32768 THEN 'SMALLINT'
 		ELSE 'KEEP as INT'
 	END AS RecommendedType
-FROM [dbo].[fds_gl_posting];
+FROM [schemaname].[tablename];
