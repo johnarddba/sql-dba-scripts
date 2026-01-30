faa62dc693e45ee5342c587df5beebca6825d10e
Johnard Hidalgo <jhidalgo@hedgeserv.com>
2026-01-30 16:35:53 +0800

    commit

---
+++ Scratch_/024.sql
@@ -0,0 +1,9 @@
+Hello,
+
+Can you please let us know if you acknowledged the below notifications reg. ENGG DR (between 21:15 and 2 AM we received 7 alerts reg. the DR Latency). 
+
+ENGG DR is considered P1 severity and should be treated as top priority.
+
+
+
+BAU issues are with higher priority compared to engineering tasks. Please note that this task should have been completed long ago so I cannot accept this explanation. 
