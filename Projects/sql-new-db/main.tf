cca8e31e9e67979a440bc138298267e36eac8650
johnarddba <johnard24@yahoo.com>
2026-02-15 12:26:15 +0800

    commit

--- Projects/sql-new-db/main.tf
+++ Projects/sql-new-db/main.tf
@@ -3,6 +3,6 @@ provider "null" {}
 resource "null_resource" "run_ansible" {
 
     provisioner "local-exec" {
-        command = "ansible-playbook -i inventory.ini playbook.yml"
+        command = "wsl ansible-playbook -i inventory.ini playbook.yml"
     }
 }
